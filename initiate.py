ef18526ed8b3ee3fe75770863e8f63e5c07c1b65
d-skyhawk <divyanshnikhil@gmail.com>
2017-05-11 02:12:48 +0530

    Updated Python Code for Contest Id

--- initiate.py
+++ initiate.py
@@ -1,7 +1,9 @@
 from urllib2 import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-my_url= 'http://codeforces.com/contest/789/problem/A'
+contestid='764'
+pid='A'
+my_url='http://codeforces.com/contest/'+contestid+'/problem/'+pid
 
 # opening up connection and grabbing the page
 uClient = uReq(my_url)
@@ -19,20 +21,25 @@ inputs = page_soup.findAll("div", {"class":"input"})
 f1 = open('testcases','w')
 
 f1.write(str(len(inputs))+"\n")
+idx = 1;
 
 for inp in inputs:
+	f = open('test'+str(idx), 'w')
 	xyz = str(inp.pre)
 	xyz = xyz.replace("<br/>","\n")
 	xyz = xyz.replace("<pre>","")
 	xyz = xyz.replace("</pre>","")
-	f1.write(xyz)
-outputs = page_soup.findAll("div", {"class":"output"})
+	f.write(xyz)
+	idx=idx+1;
 
-f2 = open('testsoln','w')
+outputs = page_soup.findAll("div", {"class":"output"})
+idx=1
 
 for op in outputs:
+	f = open('soln'+str(idx), 'w')
 	xyz = str(op.pre)
 	xyz = xyz.replace("<br/>","\n")
 	xyz = xyz.replace("<pre>","")
 	xyz = xyz.replace("</pre>","")
-	f2.write(xyz)
+	idx=idx+1
+	f.write(xyz)
